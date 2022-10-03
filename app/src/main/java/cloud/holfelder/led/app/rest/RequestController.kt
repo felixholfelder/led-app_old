c7a8855e6e0b41ede1fce4b4af0e1e7243ef2a20
Felix Holfelder <felixholfelder1@t-online.de>
2022-10-03 14:42:47 +0200

    changed build-sdk's

--- app/src/main/java/cloud/holfelder/led/app/rest/RequestController.kt
+++ app/src/main/java/cloud/holfelder/led/app/rest/RequestController.kt
@@ -6,8 +6,8 @@ import okhttp3.Request
 
 class RequestController {
     companion object {
-        private val USERNAME = ""
-        private val PASSWORD = ""
+        private val USERNAME = "LED-REST"
+        private val PASSWORD = "hidh98jlhxl"
         fun getClient(): OkHttpClient {
             return OkHttpClient().newBuilder().addInterceptor { chain ->
                 val originalRequest: Request = chain.request()
